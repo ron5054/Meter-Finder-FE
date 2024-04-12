6028d7e44287c86179cfb23b652a7aab1e109425
Yaron Levy <ron5054@gmail.com>
2024-04-12 22:50:50 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meter-finder/',
+  base: '/Meter-Finder/',
   plugins: [react()],
 })
